29940d4d8ed339143320436a20b0dc1fca718a19
Rayzxy <303274454@qq.com>
2017-08-25 16:07:01 +0800

    Project Self

---
+++ IAMDOUBLESIDES/Assets/Global/TextHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class TextHelper
+{
+    //多语言的封装
+    public static void Lang(string text)
+    {
+        
+    }
+}
---
+++ IAMDOUBLESIDES/Assets/Script/Drama/DramaPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//总的播放器
+public class DramaPlayer {
+    List<Drama> m_dramas;
+
+    public static void PlayWord()
+    {
+
+    }
+
+    public static void PlayTxt()
+    {
+
+    }
+}
